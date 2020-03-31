7d9d0065c92561b1e439525743fe2f27e38e79e9
Sjshovan <SJSHOVAN@GMAIL.COM>
2020-03-30 20:27:09 -0400

    initial

---
+++ README.md
@@ -0,0 +1,131 @@
+**Author:** [Sjshovan (LoTekkie)](https://github.com/Ap0gee)  
+**Version:** v0.9.0  
+
+
+# Omega Renamer
+
+> Official Omega private server Ashita v3 addon that fixes npc names through automatic updates.
+
+
+### Table of Contents
+
+- [Prerequisites](#prerequisites)
+- [Installation](#installation)
+- [Aliases](#aliases)
+- [Usage](#usage)
+- [Commands](#commands)
+- [Support](#support)
+- [Change Log](#change-log)
+- [Known Issues](#known-issues)
+- [TODOs](#todos)
+- [License](#license)
+
+___
+### Prerequisites
+1. [Final Fantasy 11 Online](http://www.playonline.com/ff11us/index.shtml)
+2. [Ashita v3](https://www.ashitaxi.com/)
+
+___
+### Installation
+
+This addon is part of the Omega private server ecosystem and comes with the offical Omega server install.   
+If you are using a separate Final Fantasy 11 install you will need to follow the manual install instructions below.
+
+**Manual:**
+1. Navigate to <https://github.com/Ap0gee/Ashita-OmegaRenamer>.
+2. Click on `Releases`. 
+3. Click on the `Source code (zip)` link within the latest release to download.
+4. Extract the zipped folder to `Ashita_v3/addons/`.
+5. Rename the folder to remove the version tag (`-v0.9.0`). The folder should be named `OmegaRenamer`.
+
+___
+### Aliases
+The following aliases are available to Omega Renamer commands:    
+
+**omegarenamer:** orenamer | oren   
+**reload:** r  
+**about:** a  
+**help:** h   
+
+___
+### Usage
+
+Manually load the addon by using one of the following commands:
+    
+    /addon load omegarenamer 
+    
+___    
+### Commands 
+
+**help**
+
+Displays the available Omega Renamer commands. Below are the equivalent ways of calling the command:
+
+    /omegarenamer help
+    /orenamer help
+    /oren help
+    
+    /omegarenamer h
+    /orenamer h
+    /oren h
+
+**reload**
+
+Reloads the Omega Renamer addon. Below are the equivalent ways of calling the command:
+    
+    /omegarenamer reload
+    /orenamer reload
+    /oren reload
+    
+    /omegarenamer r
+    /orenamer r
+    /oren r
+    
+**about**
+
+Displays information about the Omega Renamer addon. Below are the equivalent ways of calling the command:
+    
+    /omegarenamer about
+    /orenamer about
+    /oren about
+    
+    /omegarenamer a
+    /orenamer a
+    /oren a
+
+___
+### Support
+**Having Issues with this addon?**
+* Please let me know [here](https://github.com/Ap0gee/Ashita-OmegaRenamer/issues/new).
+  
+**Have something to say?**
+* Send me some feedback here: <sjshovan@gmail.com>
+
+**Want to stay in the loop with my work?**
+* You can follow me at: <https://twitter.com/Sjshovan>
+
+**Want to toss a coin to your modder?**
+* You can do so here: <https://www.paypal.me/Sjshovan>  
+
+___
+### Change Log
+
+**v0.9.0** - 3/30/2020
+- Initial release
+
+___
+### Known Issues
+
+- **None** 
+___    
+### TODOS
+
+- **TODO:** Consider versioning the map files.
+___
+
+### License
+
+Copyright © 2020, [Sjshovan (LoTekkie)](https://github.com/Ap0gee).
+Released under the [BSD License](LICENSE).
+
+***
---
+++ omegarenamer.lua
@@ -0,0 +1,123 @@
+--[[
+Copyright © 2020, Sjshovan (LoTekkie)
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in the
+      documentation and/or other materials provided with the distribution.
+    * Neither the name of Mount Muzzle nor the
+      names of its contributors may be used to endorse or promote products
+      derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL Sjshovan (LoTekkie) BE LIABLE FOR ANY
+DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+--]]
+
+_addon.name = "Omega Renamer"
+_addon.author = "Sjshovan (LoTekkie) sjshovan@gmail.com"
+_addon.description = 'Official Omega private server addon that fixes npc names through automatic updates.'
+_addon.version = '0.9.0'
+_addon.commands = {'/omegarenamer', '/orenamer', '/oren'}
+
+local http = require("socket.http")
+local ltn12 = require("ltn12")
+
+require("common")
+require("constants")
+require("helpers")
+
+local help = {
+    commands = {
+        buildHelpSeperator('=', 28),
+        buildHelpTitle('Commands'),
+        buildHelpSeperator('=', 28),
+        buildHelpCommandEntry('reload', 'Reload Omega Renamer.'),
+        buildHelpCommandEntry('about', 'Display information about Omega Renamer.'),
+        buildHelpCommandEntry('help', 'Display Omega Renamer commands.'),
+        buildHelpSeperator('=', 28),
+    },
+    about = {
+        buildHelpSeperator('=', 23),
+        buildHelpTitle('About'),
+        buildHelpSeperator('=', 23),
+        buildHelpTypeEntry('Name', _addon.name),
+        buildHelpTypeEntry('Description', _addon.description),
+        buildHelpTypeEntry('Author', _addon.author),
+        buildHelpTypeEntry('Version', _addon.version),
+        buildHelpSeperator('=', 23),
+    },
+}
+
+function display_help(table_help)
+    for index, command in pairs(table_help) do
+        displayResponse(command)
+    end
+end
+
+ashita.register_event('load', function()
+    local d = string.format('%s/addons/OmegaRenamer/%s/', AshitaCore:GetAshitaInstallPath(), 'data');
+    if (not ashita.file.dir_exists(d)) then
+        ashita.file.create_dir(d);
+    end
+    local response = {}
+    http.request{
+        method = "GET",
+        url = "https://xi-omega-renamer.s3.us-east-2.amazonaws.com/map.lua",
+        sink = ltn12.sink.table(response)
+    }
+    local remoteMap = table.concat(response)
+    local f = io.open(string.format('%s/%s', d, "map.lua"), 'w+');
+    if (f ~= nil) then
+        f:write(remoteMap);
+        f:close();
+    end
+    require("data.map")
+end)
+
+ashita.register_event('command', function(command, ntype)
+
+    local command_args = command:lower():args()
+        
+    if not tableContains(_addon.commands, command_args[1]) then
+        return false
+    end 
+    
+    if command_args[2] == 'reload' or command_args[2] == 'r' then
+        AshitaCore:GetChatManager():QueueCommand('/addon reload omegarenamer', 1)
+    
+    elseif command_args[2] == 'about' or command_args[2] == 'a' then
+        display_help(help.about)
+        
+    elseif command_args[2] == 'help' or command_args[2] == 'h' then
+        display_help(help.commands)
+
+    else
+        display_help(help.commands)
+    end
+
+    return false
+end)
+
+ashita.register_event('prerender', function()
+    local zoneId = AshitaCore:GetDataManager():GetParty():GetMemberZone(0);
+    local npcs = map[zoneId];
+    if (npcs ~= nil) then
+        for _, data in pairs(npcs) do
+            local npcIndex = bit.band(data[1], 0x0FFF);
+            AshitaCore:GetDataManager():GetEntity():SetName(npcIndex, data[2]);
+        end
+    end
+end)
